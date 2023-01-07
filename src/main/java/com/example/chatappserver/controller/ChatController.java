3d710e0c9b835665e0c07eec0c20fb63cd56e47d
juliosc1 <julio.siklander@gmail.com>
2023-01-07 19:16:11 +0100

    add chatController

---
+++ src/main/java/com/example/chatappserver/controller/ChatController.java
@@ -0,0 +1,22 @@
+package com.example.chatappserver.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/message") // /app/message
+    @SendTo("/chatroom/public")
+    public Message sendMessage(@Payload Message message) {
+        return message;
+    }
+}
